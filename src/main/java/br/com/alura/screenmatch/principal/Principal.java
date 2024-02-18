1377d7132c7464b02fc557c1b4cf40caf9e5c2e0
EMANUEL DA SILVA NASCENTE <106539519+enascentedev@users.noreply.github.com>
2024-02-18 09:44:06 -0300

    aula 1

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -17,7 +17,7 @@ public class Principal {
     private ConverteDados conversor = new ConverteDados();
 
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
-    private final String API_KEY = "&apikey=6585022c";
+    private final String API_KEY = "&apikey=1352f28e";
 
     public void exibeMenu(){
         System.out.println("Digite o nome da série para busca");
